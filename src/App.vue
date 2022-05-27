e277fe55d5a708169702b3d194798575207bb492
arindra97 <ar28297@gmail.com>
2022-05-27 23:39:04 +0700

    update commit

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld class="text-3xl text-pink-500" msg="I did it!" />
+      <HelloWorld class="text-3xl text-pink-500" msg="Yes, I did it!" />
     </div>
   </header>
 
